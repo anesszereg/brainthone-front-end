9a5451e011a02a1f5f52207f0c1a2f7bfeb43b87
anesszereg <anesszereg1@gmail.com>
2023-09-21 14:16:49 +0100

    :robot:

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+
+import { Box, Typography } from '@mui/material'
+import './Navbar.css'
+
+
+function Navbar() {
+  return (
+    <Box display={'flex'}>
+      <Box >
+
+      </Box>
+        <Box display={'flex'}> 
+        <Typography></Typography>
+        <Typography></Typography>
+        <Typography></Typography>
+        <Typography></Typography>
+        <Typography></Typography>
+
+        </Box>
+
+    </Box>
+  )
+}
+
+export default Navbar
