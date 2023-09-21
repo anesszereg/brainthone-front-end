dc5be7bf50456a0fb0d331b90a133a5a7f15032d
saliiimm <salimghalem40@gmmail.com>
2023-09-21 16:05:30 +0100

    home

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-
-import './App.css'
-import Navbar from './Components/Navbar/Navbar'
+import './App.css';
+import Home from './Components/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
-
-
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,48 @@
-
+import homesvg from '../../assets/Svgs/home.svg';
+import { Button, Grid } from '@mui/material';
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div style={{ width: '100%' }}>
+      <Grid style={{ width: '100%' }} container spacing={0}>
+        <Grid
+          item
+          xs={6}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+          }}>
+          <h2
+            style={{
+              margin: '0 auto',
+              fontSize: '47.11px',
+              textAlign: 'start',
+              maxWidth: '73%',
+            }}>
+            <span style={{ color: '#007AFF' }}>Play, </span>
+            <span style={{ color: '#FFBA07' }}>Learn, </span>
+            <span style={{ color: '#FF0000' }}>Thrive! </span> Gamify 🎮 Your
+            <span style={{ color: '#00D459' }}> Knowledge</span> Journey!
+          </h2>
+          <p
+            style={{ textAlign: 'start', maxWidth: '73%', fontSize: '16.8px' }}>
+            We&apos;re a team of experienced developers creating innovative web
+            and mobile solutions that empower businesses to succeed online.
+            Let&apos;s bring your vision to life
+          </p>
+          <div style={{ marginTop: '150px', marginInline: 'auto' }}>
+            <Button>Get started</Button>
+            <Button variant="text">Get more info</Button>
+          </div>
+        </Grid>
+        <Grid item xs={6}>
+          <img src={homesvg} style={{ margin: '0 auto' }} />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
