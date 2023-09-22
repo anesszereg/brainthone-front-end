5a68522e5986a335deb6df722e96a88005fce718
Wassim <512.amosimo@gmail.com>
2023-09-22 16:28:09 +0100

    Waya zarzour

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import Main from "./pages/main/Main";
 import "./App.css";
 import Auth from "./pages/auth/Auth";
+import Grade from "./pages/grade/Grade";
+import Welcome from "./pages/welcome/Welcome.jsx";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/auth" element={<Auth />} />
+        <Route path="/grade" element={<Grade />}/>
+        <Route path="/welcome" element={<Welcome/>}/>
       </Routes>
 
     </>
