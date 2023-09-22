812546b6a513b57502183995b4e4bbe601058c05
berchiche willem <berwillempro@gmail.com>
2023-09-22 08:52:58 +0100

    fixes

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,65 +1,93 @@
-import React from 'react'
-import * as yup from 'yup'
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
-import './Contact.css'
-import tfelsghir from '../../assets/Pngs/tfelsghir.png'
-
+import "./Contact.css";
+import tfelsghir from "../../assets/Pngs/tfelsghir.png";
 
 //yup schema
 const schema = yup.object({
-    email: yup.string().email("Not a valid email address.").required("Please write your email."),
-    message: yup.string().required("Message can't be empty.")
-})
+  email: yup
+    .string()
+    .email("Not a valid email address.")
+    .required("Please write your email."),
+  message: yup.string().required("Message can't be empty."),
+});
 
 function Contact() {
+  //handle submit function
+  const submitFunc = (data) => console.log(data);
 
-    //handle submit function 
-    const submitFunc = (data)=> console.log(data)
-
-    const {register, handleSubmit, formState: { errors }} = useForm({
-        resolver:yupResolver(schema)
-      })
-    
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
   return (
-    <div className='contact-wrapper'>
+    <div className="contact-wrapper">
       <h1>Contact us</h1>
       <h2>Get in touch</h2>
-      <div className='contact-card'>
+      <div className="contact-card">
+        <p
+          style={{
+            color: "#FFBA07",
+            fontSize: "0.8em",
+          }}
+        >
+          Enter Your Email Below
+        </p>
 
-        <p style={{
-            color:"#FFBA07",
-             fontSize:"0.8em"
-             }}>Enter Your Email Below</p>
+        <p
+          style={{
+            fontWeight: 600,
+            fontSize: "1.6em",
+            maxWidth: "502px",
+          }}
+        >
+          Support is just a click away. Contact us for prompt assistance with
+          your inquiries.
+        </p>
 
-        <p style={{
-            fontWeight:600,
-            fontSize:"1.6em",
-            maxWidth:"502px"
-        }}>Support is just a click away. Contact us for prompt assistance with your inquiries.</p>
+        <form className="contact-form" onSubmit={handleSubmit(submitFunc)}>
+          <input
+            type="text"
+            placeholder="Enter Your Email"
+            className="contact-input"
+            {...register("email")}
+          />
 
-        <form className='contact-form' onSubmit={handleSubmit(submitFunc)}>
+          <textarea
+            type="text"
+            placeholder="Enter Your Message"
+            className="contact-input"
+            {...register("message")}
+            style={{ height: "170px" }}
+          />
 
-            <input type='text' placeholder='Enter Your Email' className='contact-input' {...register("email")}/>
-
-            <textarea type='text' placeholder='Enter Your Message' className='contact-input' {...register("message")} style={{height:'170px'}}/>
-
-            <button className={"primary-button"+ (Object.keys(errors).length > 0 ? " contact-submit-error": "") } style={{
-                borderRadius:'4px', 
-                width:"502px", 
-                fontWeight:600, 
-                fontSize:"1.2em"
-                }}>
-                    {Object.values(errors).length > 0 ? Object.values(errors)[0].message : "Send Your Message"}
-            </button>
+          <button
+            className={
+              "primary-button" +
+              (Object.keys(errors).length > 0 ? " contact-submit-error" : "")
+            }
+            style={{
+              borderRadius: "4px",
+              width: "502px",
+              fontWeight: 600,
+              fontSize: "1.2em",
+            }}
+          >
+            {Object.values(errors).length > 0
+              ? Object.values(errors)[0].message
+              : "Send Your Message"}
+          </button>
         </form>
-
       </div>
-      <img src={tfelsghir} className='tfelsghir'></img>
+      <img src={tfelsghir} className="tfelsghir"></img>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,13 +1,17 @@
 import Advantages from "../../Components/Advantages/Advantages";
-import Home from "../../Components/Home/Home"
+import Home from "../../Components/Home/Home";
 import Navbar from "../../Components/Navbar/Navbar";
+import Contact from "../../Components/Contact/Contact";
 
 const main = () => {
-  return <>
-  <Navbar/>
-  <Home/>
-  <Advantages/>
-  </>;
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Advantages />
+      <Contact />
+    </>
+  );
 };
 
 export default main;
