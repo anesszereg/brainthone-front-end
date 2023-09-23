3289fbe29de4a6f71d323e4984d9b9ccb4e26101
saliiimm <salimghalem40@gmmail.com>
2023-09-23 09:11:00 +0100

    dashboard teacher

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-
-import { Route, Routes } from "react-router-dom";
-import Main from "./pages/main/Main";
-import "./App.css";
-import Auth from "./pages/auth/Auth";
-
+import { Route, Routes } from 'react-router-dom';
+import Main from './pages/main/Main';
+import './App.css';
+import Auth from './pages/auth/Auth';
+import TeacherDashboard from './pages/dashboard-teacher/TeacherDashboard';
 function App() {
   return (
     <>
-
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/auth" element={<Auth />} />
+        <Route path="/teacher-dashboard" element={<TeacherDashboard />} />
       </Routes>
-
     </>
   );
 }
