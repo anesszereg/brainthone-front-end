985b656918a58a0af12341374483e2bc82fc7e14
berchiche willem <berwillempro@gmail.com>
2023-09-23 02:58:52 +0100

    front-fixes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,3 @@
-
-import { Box, Typography } from "@mui/material";
-
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 
@@ -27,7 +24,6 @@ function Navbar() {
       <Link to="auth">
         <button className="primary-button login">Login</button>
       </Link>
-
     </div>
   );
 }
